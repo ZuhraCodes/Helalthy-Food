06ffdc19932be11d4b25454632233879edcbbf39
ZuhraCodes <zuhrasayfullayeva31@gmail.com>
2025-05-05 19:48:31 +0500

    Birinchih

---
+++ main.py
@@ -0,0 +1,10 @@
+mevalar = ["olma", "anor", "ananas"]
+for meva in mevalar:
+    print(meva)
+
+for son in range(11, 1, -1):
+    print(son)
+
+moshinalar = ["Tesla", "Bugatti", "BMW", "Ferrari", "Porshe"]
+
+gullar = ["pion", "atirgul", "lola", "bangidevona", "sallagul"]
